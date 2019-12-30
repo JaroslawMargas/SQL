3ca387c9684a89e181a36c7f55ef90711b85115d
BABA <jaromarg@gmail.com>
2019-12-30 16:03:34 +0100

    ANY vs ALL

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,21 +1,11 @@
 select * from dbo.Produkty_sklep2 as p2
---WHERE p2.Nazwa2 LIKE 'al%'
-
---WHERE p2.Nazwa2 LIKE '%uf%'
-
---WHERE p2.Nazwa2 LIKE '%f_lgi%'
-
---WHERE p2.Nazwa2 LIKE '[a]%'
-
---WHERE p2.Nazwa2 LIKE '[a-c]%'
-
---WHERE p2.Nazwa2 LIKE '[^a-c]%'
-
---WHERE p2.CenaJedn2 BETWEEN  5 AND 100
-
---WHERE p2.Nazwa2 IN ('Alufelgi Silver', 'Choika pachnaca')
-
-WHERE p2.CenaJedn2 > ANY (
-		select avg(p.CenaJedn)
+-- switch ANY -> ALL
+WHERE p2.KodProduktu2 > any (
+		select p.KodProduktu
 		from dbo.Produkty as p
+		where p.KodProduktu > 3
 		)
+
+-- Subquery return (4,5,6)
+-- kodProduktu > ANY (4,5,6) --> ANY 5,6,7,8,9
+-- kodProduktu > ALL (4,5,6) --> ALL = 7,8,9
