2f4340b16579e98795dfcf595751d812229af540
BABA <jaromarg@gmail.com>
2019-12-30 16:06:47 +0100

    Like, between, In, ANY vs ALL

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,6 +1,22 @@
 select * from dbo.Produkty_sklep2 as p2
+--WHERE p2.Nazwa2 LIKE 'al%'
+
+--WHERE p2.Nazwa2 LIKE '%uf%'
+
+--WHERE p2.Nazwa2 LIKE '%f_lgi%'
+
+--WHERE p2.Nazwa2 LIKE '[a]%'
+
+--WHERE p2.Nazwa2 LIKE '[a-c]%'
+
+--WHERE p2.Nazwa2 LIKE '[^a-c]%'
+
+--WHERE p2.CenaJedn2 BETWEEN  5 AND 100
+
+--WHERE p2.Nazwa2 IN ('Alufelgi Silver', 'Choika pachnaca')
+
 -- switch ANY -> ALL
-WHERE p2.KodProduktu2 > any (
+WHERE p2.KodProduktu2 > ANY (
 		select p.KodProduktu
 		from dbo.Produkty as p
 		where p.KodProduktu > 3
